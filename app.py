267417f18493bff78d6d8e26440bffa84fb8fc03
Lenitta Elvis <lenittaelvis@gmail.com>
2026-05-07 22:32:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -412,4 +412,5 @@ def style():
 # ---------------- RUN APP ----------------
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
